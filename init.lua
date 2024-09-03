c5e78b68bf01dcd1e44a8c4464be70be22556f3e
Daniel Edmiston <daniel.s.edmiston@gmail.com>
2024-09-03 17:44:04 -0400

    Install telescope

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
 
 vim.cmd("syntax on")
 vim.cmd("filetype on")
@@ -16,5 +17,11 @@ vim.cmd("nmap <C-x><Left> :bp<CR>")
 vim.cmd("map <leader>q :bp<bar>sp<bar>bn<bar>bd<CR>")
 
 require("config.lazy")
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+-- live_grep requires 'ripgrep', via e.g. 'sudo pacman -Syu ripgrep'
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,12 @@
 return {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+	{
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+    },
 }
